955712743fc06be6e74373f44a832aaa8d22be3c
suman <suman.kunwar@daliaresearch.com>
2019-12-07 17:36:31 +0545

    deploy

---
+++ precache-manifest.746b92fe5bbd9885949e598192a9e355.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "9fc2eedf080b3d06547d64bb2f00bf35",
+    "url": "/vuetify-todo-pwa-jexia/sw.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vuetify-todo-pwa-jexia/robots.txt"
+  },
+  {
+    "revision": "d1a9ade35dc509e28452",
+    "url": "/vuetify-todo-pwa-jexia/js/chunk-vendors.0bef621d.js"
+  },
+  {
+    "revision": "d4b669d0a4c3b4cfb58e",
+    "url": "/vuetify-todo-pwa-jexia/js/app.06447bb3.js"
+  },
+  {
+    "revision": "9f2c7382e6ab49d3c62b9162bdcc4937",
+    "url": "/vuetify-todo-pwa-jexia/index.html"
+  },
+  {
+    "revision": "53104a485ddaefcf1b8ee3069f38c54a",
+    "url": "/vuetify-todo-pwa-jexia/img/todo.png"
+  },
+  {
+    "revision": "856e0a97d603aa290fc3a5d20723e55b",
+    "url": "/vuetify-todo-pwa-jexia/img/pwa.png"
+  },
+  {
+    "revision": "d1a9ade35dc509e28452",
+    "url": "/vuetify-todo-pwa-jexia/css/chunk-vendors.bda1f127.css"
+  },
+  {
+    "revision": "d4b669d0a4c3b4cfb58e",
+    "url": "/vuetify-todo-pwa-jexia/css/app.6bffaf12.css"
+  }
+];
