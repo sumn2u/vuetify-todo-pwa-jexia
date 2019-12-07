b3a686625cde00a56a8e21fd7fcbc7ada6f22a22
suman <suman.kunwar@daliaresearch.com>
2019-12-07 14:43:06 +0545

    add auto refresh token

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import LocalStorageService from '@/localStorage'
 const baseURL = process.env.VUE_APP_API_BASE || ''
 
 export const ApiError = function ({
@@ -12,6 +13,56 @@ export const ApiError = function ({
   this.url = url
   this.options = options
 }
+
+const localStorageService = LocalStorageService.getService()
+// Add a request interceptor
+axios.interceptors.request.use(
+  config => {
+    const token = localStorageService.getAccessToken()
+    if (token) {
+      config.headers['Authorization'] = 'Bearer ' + token
+    }
+    // config.headers['Content-Type'] = 'application/json';
+    return config
+  },
+  error => {
+    Promise.reject(error)
+  })
+
+// Add a response interceptor
+axios.interceptors.response.use((response) => {
+  return response
+}, function (error) {
+  const originalRequest = error.config
+
+  if (error.response.status === 401 && originalRequest.url.includes(`${baseURL}/ds/todos`)) {
+    return axios.post(`${baseURL}/auth`, {
+      'method': 'apk',
+      'key': process.env.VUE_APP_JEXIA_API_KEY,
+      'secret': process.env.VUE_APP_API_SECRET
+    }).then(response => {
+      localStorageService.setToken(response.data)
+      axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorageService.getAccessToken()
+      return axios(originalRequest)
+    })
+  }
+
+  if (error.response.status === 401 && !originalRequest._retry) {
+    originalRequest._retry = true
+    const refreshToken = localStorageService.getRefreshToken()
+    return axios.post(`${baseURL}/auth/refresh`, {
+      'refresh_token': refreshToken
+    })
+      .then(response => {
+        if (response.status === 201) {
+          localStorageService.setToken(response.data)
+          axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorageService.getAccessToken()
+          return axios(originalRequest)
+        }
+      })
+  }
+  return Promise.reject(error)
+})
 
 const makeRequest = (method, url, options, request) => {
   const headers = {
